d43edf0d89f743ed8acadcc348c652d2006da7aa
maefkes <122538865+maefkes@users.noreply.github.com>
2025-11-04 09:33:44 +0100

    updatet folder structure

--- src/robot/UR3eRobot.py
+++ src/robot/UR3eRobot.py
@@ -3,11 +3,12 @@
 
 class UR3eRobot:
 
-    def __init__(self, rtdeControl, rtdeReceive, robotiqGripper, homePosition):
+    def __init__(self, rtdeControl, rtdeReceive, robotiqGripper, settingsManager, homePosition):
 
         self.rtdeControl = rtdeControl
         self.rtdeReceive = rtdeReceive
         self.robotiqGripper = robotiqGripper
+        self.manager = settingsManager
         self.homePosition = homePosition
         
         self.moveL(self.homePosition, 0.3, 0.1)
@@ -18,7 +19,7 @@ class UR3eRobot:
     def moveL(self, position, speed, acc):
         self.rtdeControl.moveL(position, speed, acc)
         
-     def disconnect(self):
+    def disconnect(self):
         self.rtdeControl.disconnect()
         self.rtdeReceive.disconnect()
         
