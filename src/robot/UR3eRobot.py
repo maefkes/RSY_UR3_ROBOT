25372270e0e0f40d11ce427e140492f5869dcd8e
febe245 <barking.felix@gmx.de>
2025-11-04 09:51:09 +0100

    add MoveJ in Robot class

--- src/robot/UR3eRobot.py
+++ src/robot/UR3eRobot.py
@@ -1,5 +1,6 @@
 
-
+import time
+import math
 
 class UR3eRobot:
 
@@ -13,12 +14,33 @@ class UR3eRobot:
         
         self.moveL(self.homePosition, 0.3, 0.1)
 
+    def move_joint_axis(self, axis_index, delta_angle):
+    
+        """
+        SPAETER ENTFERNEN
+        """
+        speed = 0.3
+        acc = 0.8
+        current_joints = self.getActualQ()
+        
+        target_joints = list(current_joints)
+        target_joints[axis_index] += delta_angle
+
+        print(f"Fahre Achse {axis_index+1} um {math.degrees(delta_angle):.1f}°...")
+        self.moveJ(target_joints, speed, acc)
+        time.sleep(0.5)
+
     def getActualTCPPose(self):
         return  self.rtdeReceive.getActualTCPPose()
     
     def moveL(self, position, speed, acc):
         self.rtdeControl.moveL(position, speed, acc)
 
+    def moveJ(self, position, speed, acc):
+        self.rtdeControl.moveJ(position, speed, acc)
+
+    def getActualQ(self):
+        return self.rtdeReceive.getActualQ()
 
     def disconnect(self):
         self.rtdeControl.disconnect()
