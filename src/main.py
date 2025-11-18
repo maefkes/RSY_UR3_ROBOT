eecebe902e98385d00d12e0a1e0631ecd64a4aa5
maefkes <122538865+maefkes@users.noreply.github.com>
2025-11-18 10:00:06 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,52 +1,19 @@
 import sys
-import socket
-import time
-import math
-
-from UR3eRobotInitialisation import robot3, robot4
+import os
+from PyQt6 import QtWidgets
+from gui import MainWindow
 
 
+SIMULATION = os.environ.get('SIMULATION', 'False') == 'True'
 
-
-
-def testMain():
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit(app.exec())
     
-        # === Bewegungsparameter ===
-    speed = 0.3         # rad/s
-    acceleration = 0.8  # rad/s²
-
-    # === Aktuelle Gelenkposition holen ===
-    joints = robot3.getActualQ()
-    print("Aktuelle Gelenkwinkel [rad]:", [round(j, 3) for j in joints])
-
-        # === Beispielbewegungen ===
-    # +10° auf Achse 1
-    robot3.move_joint_axis(0, math.radians(5))
-
-    # -5° auf Achse 2
-    robot3.move_joint_axis(1, math.radians(5))
-
-    # +15° auf Achse 6
-    robot3.move_joint_axis(5, math.radians(5))
-
-    home_joints = [0, -1.5708, 0, -1.5708, 0, 0]
-    robot3.moveJ(home_joints, speed, acceleration)
-
-    robot3.move_joint_axis(5, math.radians(90))
-
-    robot3.move_joint_axis(5, math.radians(-90))
-
-    # === Zur Ausgangsposition zurückkehren ===
-    print("Zur Ausgangsposition zurückkehren...")
-    robot3.moveJ(joints, speed, acceleration)
-
-    
-    
-    
-    
-
 ############################################################################################
 #   STARTE PROGRAMM
 ############################################################################################
 if __name__ == "__main__":
-    testMain()
+    main()
